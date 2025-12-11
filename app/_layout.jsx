9c4e1f2d7678e4ebf9575bfdbfc23a29138aab0c
Jesmin Chakma <jesmin.webappick@gmail.com>
2025-12-11 13:48:49 +0600

    Layout added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import "../global.css";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
   return (
-    <>
-      <StatusBar style="auto" />
-    </>
+    
+     <ThemeProvider value={useColorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ drawerLabel: "Home", headerShown:false }} />
+          <Stack.Screen name="dashboard" options={{ drawerLabel: "Dashboard" }} />
+        </Stack>
+
+        <StatusBar style="auto" />
+      </ThemeProvider>
+
+    
   );
 }
