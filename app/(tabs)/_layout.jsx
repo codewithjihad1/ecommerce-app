7d47afce21b6d8a2a0e9e124b464d42305d6b4f6
afiquewebappick <afique.webappick@gmail.com>
2025-12-15 09:25:54 +0600

    slider

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,25 +1,26 @@
 import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
-    return (
-        <Tabs
-            screenOptions={{
-                headerShown: false,
-            }}>
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: 'Home',
-                    // tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="explore"
-                options={{
-                    title: 'Explore',
-                    // tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-                }}
-            />
-        </Tabs>
-    );
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          // tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          // tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
