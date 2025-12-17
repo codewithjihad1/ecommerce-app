7bdb8e9cf2780b7d575801f816004d09ad1d66dc
afiquewebappick <afique.webappick@gmail.com>
2025-12-17 09:52:14 +0600

    fix flash sale container

--- src/components/home/FlashSale/FlashSale.jsx
+++ src/components/home/FlashSale/FlashSale.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, Image, FlatList } from 'react-native';
+import { View, Text, Image, FlatList, Pressable } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import bagImg from '../../../../assets/shopCategoriesImage/bag.png';
 import { LinearGradient } from 'expo-linear-gradient';
+import { useRouter } from 'expo-router';
 
 const flashProducts = [
   { id: 1, src: bagImg },
@@ -14,16 +15,25 @@ const flashProducts = [
 ];
 
 const FlashSale = () => {
+  const router = useRouter();
 
   return (
     <View className="mt-4 bg-[#FFFFFF]">
       <View className="flex-row justify-between items-center">
-        <Text
-          className="text-2xl font-bold"
-          style={{ fontFamily: 'RalewayBold' }}
+        <Pressable
+          onPress={() => {
+            router.push({
+              pathname: '/flashSale',
+            });
+          }}
         >
-          Flash Sale
-        </Text>
+          <Text
+            className="text-2xl font-bold"
+            style={{ fontFamily: 'RalewayBold' }}
+          >
+            Flash Sale
+          </Text>
+        </Pressable>
 
         <View className="flex-row gap-x-3 items-center">
           <MaterialIcons name="access-alarm" size={24} color="#004CFF" />
@@ -49,7 +59,7 @@ const FlashSale = () => {
           renderItem={({ item }) => (
             <View className="shadow-md rounded-lg bg-[#FFFFFF] mb-1 relative">
               <Image
-                className="w-[110px] h-[110px] border-[6px] border-white rounded-lg"
+                className="w-[120px] h-[130px] border-[6px] border-white rounded-lg"
                 source={item.src}
               ></Image>
 
@@ -58,7 +68,7 @@ const FlashSale = () => {
                   colors={['#F91949', '#FE528A']}
                   start={{ x: 0, y: 0 }}
                   end={{ x: 1, y: 0 }}
-                  style={{borderRadius: 6}}
+                  style={{ borderRadius: 6 }}
                 >
                   <Text className="text-white text-xs font-bold px-1.5 py-1">
                     -20%
