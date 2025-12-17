d09d6e96a7b5361ae94bf54c3191a01acd8b2ab3
MD Jihad Hossain <mdjihadhossain793@gmail.com>
2025-12-17 10:38:23 +0600

    fix: update login redirection and improve error logging; refactor layout and tailwind config

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,45 +1,45 @@
-import { store } from "@/src/store/index";
-import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import "react-native-reanimated";
-import { Provider } from "react-redux";
-import "../global.css";
+import { store } from '@/src/store/index';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { Provider } from 'react-redux';
+import '../global.css';
 
 // Prevent splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync();
 
 export const unstable_settings = {
-  anchor: "(tabs)",
+    anchor: '(tabs)',
 };
 
 export default function RootLayout() {
-  const [fontsLoaded, fontsError] = useFonts({
-    Poppins: require("../assets/font/Poppins-Regular.ttf"),
-    RalewayBold:require("../assets/font/Raleway-Bold.ttf"),
-    Raleway: require("../assets/font/Raleway-Regular.ttf"),
-    PoppinsBold: require("../assets/font/Poppins-Bold.ttf"),
-  });
+    const [fontsLoaded, fontsError] = useFonts({
+        Poppins: require('../assets/font/Poppins-Regular.ttf'),
+        RalewayBold: require('../assets/font/Raleway-Bold.ttf'),
+        Raleway: require('../assets/font/Raleway-Regular.ttf'),
+        PoppinsBold: require('../assets/font/Poppins-Bold.ttf'),
+    });
 
-  useEffect(() => {
-    // console.log(fontsError);
-    if (fontsLoaded || fontsError) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        // console.log(fontsError);
+        if (fontsLoaded || fontsError) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontsError]);
+
+    if (!fontsLoaded && !fontsError) {
+        return null;
     }
-  }, [fontsLoaded, fontsError]);
 
-  if (!fontsLoaded && !fontsError) {
-    return null;
-  }
-
-  return (
-    <Provider store={store}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="product" options={{headerShown:false }} />
-<Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      </Stack>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                {/* <Stack.Screen name="product" options={{ headerShown: false }} />
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} /> */}
+            </Stack>
+        </Provider>
+    );
 }
