70d09063b88c6828992201cdc49887df27351358
Jesmin Chakma <jesmin.webappick@gmail.com>
2025-12-30 12:02:40 +0600

    Refactor AuthScreen layout and improve code readability; add Products page with filtering functionality

---
+++ app/product/products/index.jsx
@@ -0,0 +1,134 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { useLocalSearchParams } from "expo-router";
+import { useRouter, useSearchParams } from "expo-router/build/hooks";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useSanityProducts } from "../../../src/components/hooks/useSanityProducts";
+
+export default function Products() {
+    const { type } = useLocalSearchParams();
+    const { from } = useSearchParams();
+    const router = useRouter();
+
+    const { products } = useSanityProducts();
+    console.log(products);
+
+    const handleGoBack = () => {
+        if (from) {
+            router.replace(`${from}`);
+        } else if (router.canGoBack()) {
+            router.back();
+        } else {
+            router.replace("/");
+        }
+    };
+
+    // filter the product by product type
+    const filterProductsByProductType = products.filter(
+        (product) => product.producttype.toLowerCase() === type.toLowerCase(),
+    );
+
+    console.log(filterProductsByProductType);
+
+    return (
+        <SafeAreaView className="flex-1 bg-white">
+            {/* Header */}
+            <View className="mx-5 mt-2 flex-row items-center">
+                <TouchableOpacity
+                    onPress={handleGoBack}
+                    className="flex-row items-center"
+                >
+                    <MaterialIcons
+                        className="rounded-full bg-gray-300 p-2"
+                        name="keyboard-arrow-left"
+                        size={24}
+                        color="black"
+                    />
+                </TouchableOpacity>
+            </View>
+
+            {/* product type */}
+            <View className="mx-5">
+                <Text
+                    className="text-center text-2xl font-bold"
+                    style={{ fontFamily: "RalewayBold" }}
+                >
+                    {type.toUpperCase()}
+                </Text>
+            </View>
+
+            <FlatList
+                data={filterProductsByProductType}
+                numColumns={2}
+                keyExtractor={(item) => item._id}
+                columnWrapperStyle={{
+                    justifyContent: "space-between",
+                    paddingHorizontal: 20,
+                }}
+                renderItem={({ item }) => {
+                    const slug = item.subcategoryName?.toLowerCase();
+                    return (
+                        <TouchableOpacity
+                            style={{ maxWidth: "50%" }}
+                            className="m-2 flex-1 p-3"
+                            onPress={() =>
+                                router.push({
+                                    pathname: "/product/[slug]/[id]",
+                                    params: {
+                                        slug,
+                                        id: item._id,
+                                    },
+                                })
+                            }
+                        >
+                            {/* Image Container */}
+                            <View className="mb-2 h-40 w-full">
+                                <Image
+                                    source={{ uri: item.image }}
+                                    style={{ width: "100%", height: "100%" }} // Fix: Image should be 100% of the card
+                                    resizeMode="contain"
+                                />
+                            </View>
+
+                            <Text
+                                style={{ fontFamily: "RalewayBold" }}
+                                numberOfLines={1}
+                                className="text-sm"
+                            >
+                                {item.title}
+                            </Text>
+
+                            <Text className="font-semibold text-blue-600">
+                                ${item.price}
+                            </Text>
+
+                            <Text className="py-1 text-[10px] text-gray-500">
+                                Sizes: {item.sizes?.join(", ")}
+                            </Text>
+
+                            {/* Colors Section */}
+                            <View className="mt-1 flex-row gap-1">
+                                {item.colors?.map((color, index) => (
+                                    <View
+                                        key={index}
+                                        style={{
+                                            backgroundColor: color
+                                                .trim()
+                                                .toLowerCase(),
+                                            width: 14,
+                                            height: 14,
+                                            borderRadius: 7,
+                                            borderWidth: 1,
+                                            borderColor: "#E5E7EB",
+                                        }}
+                                    />
+                                ))}
+                            </View>
+                        </TouchableOpacity>
+                    );
+                }}
+                contentContainerStyle={{ paddingBottom: 20 }}
+            />
+        </SafeAreaView>
+    );
+}
